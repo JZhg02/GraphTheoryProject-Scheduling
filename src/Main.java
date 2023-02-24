6a2bb0b00761a281940115ded71ab32325d67560
JZhg02 <94388221+JZhg02@users.noreply.github.com>
2023-02-24 18:51:56 +0100

    test files

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
 
-        Graph graph = new Graph("file1.txt");
+        Graph graph = new Graph("table9.txt");
         System.out.println("Adjacent Matrix:\n"+graph);
 
     }
